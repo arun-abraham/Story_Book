4fe81feb3c3427be7861717140537db5cd47fc60
Sam Trapp <set9069@rit.edu>
2014-09-03 16:37:16 -0400

    Starting noun noun verb connection. Broken.

---
+++ Assets/Scripts/Noun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Noun : MonoBehaviour {
+	public bool possibleSubject;
+	public List<int> a;
+	[SerializeField]
+	public Dictionary<Noun, Verb> objectVerbs;
+	public List<KeyValuePair<Noun, Verb>> objectDefaultVerbs;
+
+	void Start () 
+	{
+		if (objectVerbs == null)
+		{
+			//objectVerbs = new List<KeyValuePair<Noun, Verb>>();
+		}
+		objectDefaultVerbs = new List<KeyValuePair<Noun, Verb>>();
+		ResetDefaultVerbs();
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	private void ResetDefaultVerbs() 
+	{
+		objectDefaultVerbs.Clear();
+		for (int i = 0; i < objectVerbs.Count; i++) 
+		{
+			//objectDefaultVerbs.Add(objectVerbs[i]);
+		}
+	}
+
+	public class ObjectVerb
+	{
+		public Noun obj;
+		public Verb verb;
+	}
+}
---
+++ Assets/Scripts/Verb.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Verb : MonoBehaviour {
+	public string action;
+}
