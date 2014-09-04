b68ecb0b1f98455a9e7e843e37f5bd05a05b2978
Sam Trapp <set9069@rit.edu>
2014-09-03 20:56:30 -0400

    Increment objects in list and update story.

---
+++ Assets/Scripts/GlobalInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GlobalInput : MonoBehaviour {
+    public TextMesh storyText;
+    public List<Noun> subjects;
+    public int subjectIndex = 0;
+    public List<Noun> objects;
+    public int objectIndex = 0;
+
+    void Start()
+    {
+        UpdateStory();
+    }
+
+    void Update()
+    {
+        // Increment target object.
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            objectIndex++;
+            if (objects[objectIndex] == subjects[subjectIndex])
+            {
+                objectIndex++;
+            }
+            if (objectIndex > objects.Count)
+            {
+                objectIndex = 0;
+            }
+
+            UpdateStory();
+        }
+    }
+
+    private void UpdateStory()
+    {
+        storyText.text = subjects[subjectIndex].id + " " + subjects[subjectIndex].FindObjectConnection(objects[objectIndex]).action + " " + objects[objectIndex].id;
+    }
+}
--- Assets/Scripts/Noun.cs
+++ Assets/Scripts/Noun.cs
@@ -3,19 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Noun : MonoBehaviour {
+    public string id;
 	public bool possibleSubject;
-	public List<int> a;
-	[SerializeField]
-	public Dictionary<Noun, Verb> objectVerbs;
-	public List<KeyValuePair<Noun, Verb>> objectDefaultVerbs;
+	public List<ObjectVerb> objectVerbs;
+    [HideInInspector]
+	public List<ObjectVerb> objectDefaultVerbs;
 
 	void Start () 
 	{
 		if (objectVerbs == null)
 		{
-			//objectVerbs = new List<KeyValuePair<Noun, Verb>>();
+            objectVerbs = new List<ObjectVerb>();
 		}
-		objectDefaultVerbs = new List<KeyValuePair<Noun, Verb>>();
+        objectDefaultVerbs = new List<ObjectVerb>();
 		ResetDefaultVerbs();
 	}
 
@@ -29,10 +29,24 @@ public class Noun : MonoBehaviour {
 		objectDefaultVerbs.Clear();
 		for (int i = 0; i < objectVerbs.Count; i++) 
 		{
-			//objectDefaultVerbs.Add(objectVerbs[i]);
+			objectDefaultVerbs.Add(objectVerbs[i]);
 		}
 	}
 
+    public Verb FindObjectConnection(Noun obj)
+    {
+        Verb connection = null;
+        for (int i = 0; i < objectVerbs.Count && connection == null; i++)
+        {
+            if (objectVerbs[i].obj = obj)
+            {
+                connection = objectVerbs[i].verb;
+            }
+        }
+        return connection;
+    }
+
+    [System.Serializable]
 	public class ObjectVerb
 	{
 		public Noun obj;
