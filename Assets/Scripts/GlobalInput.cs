25adf0d61c7c39ecf0c18781bca4ceb96cd98d6f
Sam Trapp <set9069@rit.edu>
2014-09-03 22:32:30 -0400

    Replicate action from previous verb, or use default of current verb.

--- Assets/Scripts/GlobalInput.cs
+++ Assets/Scripts/GlobalInput.cs
@@ -3,38 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GlobalInput : MonoBehaviour {
-    public TextMesh storyText;
-    public List<Noun> subjects;
-    public int subjectIndex = 0;
-    public List<Noun> objects;
-    public int objectIndex = 0;
+	public TextMesh storyText;
+	public List<Noun> subjects;
+	public int subjectIndex = 0;
+	public List<Noun> objects;
+	public int objectIndex = 0;
 
-    void Start()
-    {
-        UpdateStory();
-    }
+	void Start()
+	{
+		UpdateStory();
+	}
 
-    void Update()
-    {
-        // Increment target object.
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            objectIndex++;
-            if (objects[objectIndex] == subjects[subjectIndex])
-            {
-                objectIndex++;
-            }
-            if (objectIndex > objects.Count)
-            {
-                objectIndex = 0;
-            }
+	void Update()
+	{
+		// Increment target object.
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			Verb connection = subjects[subjectIndex].FindObjectConnection(objects[objectIndex]);
+			objectIndex++;
+			if (objectIndex >= objects.Count)
+			{
+				objectIndex = 0;
+			}
+			if (objects[objectIndex] == subjects[subjectIndex])
+			{
+				objectIndex++;
+			}
 
-            UpdateStory();
-        }
-    }
+			// If the object is not being replaced, change the verb action to default of new object.
+			if (!Input.GetKey(KeyCode.LeftShift))
+			{
+				connection = subjects[subjectIndex].FindObjectDefaultConnection(objects[objectIndex]);
+			}
 
-    private void UpdateStory()
-    {
-        storyText.text = subjects[subjectIndex].id + " " + subjects[subjectIndex].FindObjectConnection(objects[objectIndex]).action + " " + objects[objectIndex].id;
-    }
+			subjects[subjectIndex].SetObjectConnection(objects[objectIndex], connection);
+			
+			UpdateStory();
+		}
+	}
+
+	private void UpdateStory()
+	{
+		storyText.text = subjects[subjectIndex].id + " " + subjects[subjectIndex].FindObjectConnection(objects[objectIndex]).action + " " + objects[objectIndex].id;
+	}
 }
--- Assets/Scripts/Noun.cs
+++ Assets/Scripts/Noun.cs
@@ -3,20 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Noun : MonoBehaviour {
-    public string id;
+	public string id;
 	public bool possibleSubject;
 	public List<ObjectVerb> objectVerbs;
-    [HideInInspector]
+	[HideInInspector]
 	public List<ObjectVerb> objectDefaultVerbs;
 
 	void Start () 
 	{
 		if (objectVerbs == null)
 		{
-            objectVerbs = new List<ObjectVerb>();
+			objectVerbs = new List<ObjectVerb>();
 		}
-        objectDefaultVerbs = new List<ObjectVerb>();
-		ResetDefaultVerbs();
+		objectDefaultVerbs = new List<ObjectVerb>();
+		ResetDefaultVerbsToCurrent();
 	}
 
 	void Update () 
@@ -24,32 +24,94 @@ public class Noun : MonoBehaviour {
 		
 	}
 
-	private void ResetDefaultVerbs() 
+	private void ResetDefaultVerbsToCurrent() 
 	{
-		objectDefaultVerbs.Clear();
 		for (int i = 0; i < objectVerbs.Count; i++) 
 		{
-			objectDefaultVerbs.Add(objectVerbs[i]);
+			if (i >= objectDefaultVerbs.Count)
+			{
+				objectDefaultVerbs.Add(new ObjectVerb(objectVerbs[i]));
+			} 
+			else
+			{
+				objectDefaultVerbs[i] = new ObjectVerb(objectVerbs[i]);
+			}
+			
 		}
 	}
 
-    public Verb FindObjectConnection(Noun obj)
-    {
-        Verb connection = null;
-        for (int i = 0; i < objectVerbs.Count && connection == null; i++)
-        {
-            if (objectVerbs[i].obj = obj)
-            {
-                connection = objectVerbs[i].verb;
-            }
-        }
-        return connection;
-    }
+	private void ResetVerbsToDefault()
+	{
+		objectVerbs.Clear();
+		for (int i = 0; i < objectDefaultVerbs.Count; i++)
+		{
+			if (i >= objectVerbs.Count)
+			{
+				objectVerbs.Add(new ObjectVerb(objectDefaultVerbs[i]));
+			}
+			else
+			{
+				objectVerbs[i] = new ObjectVerb(objectDefaultVerbs[i]);
+			}
+		}
+	}
 
-    [System.Serializable]
+	public Verb FindObjectConnection(Noun obj)
+	{
+		Verb connection = null;
+		for (int i = 0; i < objectVerbs.Count && connection == null; i++)
+		{
+			if (objectVerbs[i].obj == obj)
+			{
+				connection = objectVerbs[i].verb;
+			}
+		}
+		return connection;
+	}
+
+	public Verb FindObjectDefaultConnection(Noun obj)
+	{
+		Verb connection = null;
+		for (int i = 0; i < objectDefaultVerbs.Count && connection == null; i++)
+		{
+			if (objectDefaultVerbs[i].obj == obj)
+			{
+				connection = objectDefaultVerbs[i].verb;
+			}
+		}
+		return connection;
+	}
+
+	public Verb SetObjectConnection(Noun obj, Verb connection)
+	{
+		bool setConnection = false;
+		for (int i = 0; i < objectVerbs.Count && !setConnection; i++)
+		{
+			if (objectVerbs[i].obj == obj)
+			{
+				objectVerbs[i].verb = connection;
+				setConnection = true;
+			}
+		}
+		return connection;
+	}
+
+	[System.Serializable]
 	public class ObjectVerb
 	{
 		public Noun obj;
 		public Verb verb;
+
+		public ObjectVerb(Noun obj, Verb verb)
+		{
+			this.obj = obj;
+			this.verb = verb;
+		}
+
+		public ObjectVerb(ObjectVerb original)
+		{
+			this.obj = original.obj;
+			this.verb = original.verb;
+		}
 	}
 }
