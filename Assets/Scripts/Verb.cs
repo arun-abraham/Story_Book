df42e713bb154cf8c86bc61f49d13ecabc670bd4
Sam Trapp <set9069@rit.edu>
2014-09-06 00:51:56 -0400

    Starting VerbTag system.

--- Assets/Scripts/Verb.cs
+++ Assets/Scripts/Verb.cs
@@ -1,6 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Verb : MonoBehaviour {
 	public string action;
+	public List<VerbTag> tags;
+}
+
+[System.Serializable]
+public class VerbTag
+{
+	public enum Relationship
+	{
+		DEPENDS = 0,
+		INTERRUPTS
+	}
+
+	public string tagName;
+	public Relationship relationship;
 }
