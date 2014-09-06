5ed8cdcc543c4949089e420a3d37081f144c6f8a
Bryan O'Neill <bro9918@g.rit.edu>
2014-09-06 18:09:19 -0400

    Narrative script components added

---
+++ Assets/Scripts/Narrative.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Narrative : MonoBehaviour {
+
+	public string narText = "";
+	public Narrative nextNar;
+
+	// Use this for initialization
+	void Start () {
+		/*List<string> narText = new List<string>();
+
+		// Intro Snippet
+		narText.Add("Grandfather: You've been moping around lately, what's got you down sonny?");
+		narText.Add("Grandson: I missed the bus and had to walk all the way to school, so I was late and the teacher was mad.");
+		narText.Add("Grandfather: So?");
+		narText.Add("Grandson: and it was raining the whole way there!");
+		narText.Add("Grandfather: That’s nothing. Sit down and let me tell you what it was like to get to school in my day.");
+
+		// Page 1 Snippet
+		narText.Add("Grandfather: Back in my day, we used to have to walk to school every day, and it was uphill both ways!");
+		narText.Add("Grandson: Both ways? That doesn’t make any sense.");
+		narText.Add("Grandfather: Oh it will, just you listen.");
+
+		// Page 2 Snippet
+		narText.Add("Grandfather: Sometimes there were walls in the way, walls that went on for miles!");
+		narText.Add("Grandson: Why would someone block the way to school?");
+		narText.Add("Grandfather: To keep the wolves out, of course.");
+
+		// Page 2 After wall is removed Snippet
+		narText.Add("Grandson: So how did you get past it?");
+		narText.Add("Grandfather: Easy! I just pretended it wasn’t even there.");
+
+		// Page 3 Snippet
+		narText.Add("Grandfather: Then I was attacked by a pack of wolves! I told you those walls were there for a reason.");
+
+		// Page 3 Continuing without wall Snippet
+		narText.Add("Grandfather: and then I died!");
+		narText.Add("Grandson: ...you died, Grandpa?");
+		narText.Add("Grandfather: Yeah! Wait...hmm, maybe that wasn’t quite how it went. Now, where was I?");
+
+		// Page 3 Wall is placed Snippet
+		narText.Add("Grandfather: Take that you cowards! I could take any one of you one on one!");
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
